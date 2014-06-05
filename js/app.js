782b6526c26fa8d8f088e0ed40777baccb79028d
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2014-06-05 16:16:27 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     var secretNumber = 0;
     var userGuess = 0;
     var guessCount = 0;
+    var finish = false;
 
 /*--- This is the random number generator --*/
     function secretNumberGenerator() {
@@ -18,6 +19,7 @@ $(document).ready(function(){
     function positiveAmount() {
         if (userGuess / secretNumber === 1){
             setFeedback("You win");
+            finish = true;
           } else if ((secretNumber - userGuess) > 60.5){
             setFeedback("Wow! You are freezing!");
             } else if ((secretNumber - userGuess) > 55.5){
@@ -29,13 +31,13 @@ $(document).ready(function(){
             } else if ((secretNumber - userGuess) > 30.5) {
             setFeedback("It's getting warm around here");
             } else if((secretNumber - userGuess) > 20.5) {
-            setFeedback("It's gettin a little bit warmer");
-            } else if((secretNumber - userGuess) > 15.5) {
             setFeedback("It's getting very warm in here!");
-            } else if ((secretNumber - userGuess) > 10.5){
-            setFeedback("It's getting hot in here!");
+            } else if((secretNumber - userGuess) > 15.5) {
+            setFeedback("It's getting very very warm in here!");
+            } else if ((secretNumber - userGuess) > 7.5){
+            setFeedback("It's hot!");
             } else if ((secretNumber - userGuess) > 5.5){
-            setFeedback("It is scorching hot!");
+            setFeedback("It is very hot here!");
            } else if ((secretNumber - userGuess) > 1.5){
             setFeedback("I am burning here!!");
             } else if ((secretNumber - userGuess) > 0.5){
@@ -48,6 +50,7 @@ $(document).ready(function(){
     function negativeAmount() {
         if (userGuess / secretNumber === 1){
             setFeedback("You win");
+            finish = true;
         } else if ((userGuess - secretNumber) > 60.5){
             setFeedback("Wow! You are freezing!");
         } else if ((userGuess - secretNumber) > 55.5){
@@ -61,15 +64,16 @@ $(document).ready(function(){
         } else if((userGuess - secretNumber) > 20.5) {
             setFeedback("It's getting very warm in here!");
         } else if((userGuess - secretNumber) > 15.5) {
-            setFeedback("It's getting hot in here!");
-        } else if ((userGuess - secretNumber) > 10.5){
-            setFeedback("It is scorching hot!");
+            setFeedback("It's getting very very warm in here!");
+        } else if ((userGuess - secretNumber) > 7.5){
+            setFeedback("It is hot!");
         } else if ((userGuess - secretNumber) > 5.5){
+            setFeedback("I am very hot here!!");
+        } else if((userGuess - userGuess) > 1.5){
             setFeedback("I am burning here!!");
-        } else if ((userGuess - secretNumber) > 0.5){
+        }else if ((userGuess - secretNumber) > 0.5){
             setFeedback("Its is hotter than the sun!!!!!");
         } else {
-
         }
     }
 
@@ -95,6 +99,8 @@ $(document).ready(function(){
 /*--- Function that resets game --*/
     function newGame(){
         guessCount = 0;
+        finish = false;
+        $('#userGuess').val('');
         $('#count').text(guessCount);
         $('#guessList li').remove();
         secretNumber = (Math.floor(Math.random()*100));
@@ -118,17 +124,34 @@ $(document).ready(function(){
         newGame();
     });
 
-/*--- To get user's input --*/
-    $("form").submit(function(event){
-        event.preventDefault();
-        userGuess = $('#userGuess').val();
+/*-- Checks the user's input--*/
+function checkInput(){
+    if(isNaN(userGuess)) {
+        alert("Please enter a number from 1 - 100!");
+    } else if(userGuess === " ") {
+        alert("Well, you have to input a number");
+    } else if (userGuess < 0 || userGuess > 100) {
+        alert("Plese enter a number from 1 - 100!");
+    } else {
         comparisonAmount();
         console.log("User guess = " + userGuess);
         $('#userGuess').val('');
         guessCount++;
         setCount(guessCount);
         $('ul#guessList').append("<li>" + userGuess + "</li>");
+    }
+}
+
+/*--- To get user's input --*/
+    $("form").submit(function(event){
+        event.preventDefault();
+        //if user finished the game, it doesn't allow him to continue!
+        if(!finish){
+            userGuess = $('#userGuess').val();
+            checkInput();
+        } else {
+            setFeedback("You already won! Need to start a new game!");
+        }
     });
 });
-
 
